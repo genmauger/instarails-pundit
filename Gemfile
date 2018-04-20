a843ad2eb743a43353e6302fd7d3db44fed5b5b7
Genevieve Mauger <genevieve_mauger@live.com.au>
2018-04-20 11:13:27 +1000

    Latest version

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'jbuilder', '~> 2.5'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.7'
+  gem 'dotenv-rails'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   # Adds support for Capybara system testing and selenium driver
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should have matching email' do
+  user = User.new
+  user.email = ENV.fetch('USER_EMAIL
+    ')
+    expect(user.email).to eq('genevieve_mauger@live.com.au')
 end
+
+#seeds.rb
+#user_spec.rb
